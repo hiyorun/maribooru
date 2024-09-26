8e293b247b514cfd1cd663de2428ae305ab1013e
hiyorun <hiyorrun@e.email>
2024-09-27 00:56:04 +0700

    fix: routes with trailing slashes (/)

--- api/routes/settings.go
+++ api/routes/settings.go
@@ -5,5 +5,5 @@ import "maribooru/internal/handlers"
 func (av *VersionOne) Settings() {
 	handler := handlers.NewSettingsHandler(av.db, av.cfg, av.log)
 	settings := av.api.Group("/settings")
-	settings.GET("/", handler.Get)
+	settings.GET("", handler.Get)
 }
--- api/routes/users.go
+++ api/routes/users.go
@@ -12,11 +12,11 @@ func (av *VersionOne) Users() {
 	user.POST("/sign-up", handler.SignUp)
 	user.POST("/init-admin-create", handler.InitialCreateAdmin)
 	user.PUT("/change-password", handler.ChangePassword, av.mw.JWTMiddleware())
-	user.GET("/", handler.SelfGet, av.mw.JWTMiddleware())
-	user.PUT("/", handler.SelfUpdate, av.mw.JWTMiddleware())
-	user.DELETE("/", handler.SelfDelete, av.mw.JWTMiddleware())
+	user.GET("", handler.SelfGet, av.mw.JWTMiddleware())
+	user.PUT("", handler.SelfUpdate, av.mw.JWTMiddleware())
+	user.DELETE("", handler.SelfDelete, av.mw.JWTMiddleware())
 
 	users := av.api.Group("/users")
 	users.GET("/:id", handler.GetUserByID)
-	users.GET("/", handler.GetAllUsers)
+	users.GET("", handler.GetAllUsers)
 }
